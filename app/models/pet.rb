38bfc3bca3429390083d4008e3a45c66f941f90e
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-19 14:29:48 +0900

    モデルにバリデーション・アソシエーション記述
    各関係モデルへのアソシエーションも記述

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,17 @@
 class Pet < ApplicationRecord
 
+  with_options presence: true do
+    validates :name
+    validates :pet_type_id
+    validates :feature
+    validates :birthday
+    validates :age
+    validates :user
+  end
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :pet_type
+
 end
