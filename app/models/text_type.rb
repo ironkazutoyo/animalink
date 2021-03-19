0022ad334a2c8839b60e5a6599520e6f5d6da8d6
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-19 11:35:00 +0900

    投稿詳細表示機能の実装

--- app/models/text_type.rb
+++ app/models/text_type.rb
@@ -1,12 +1,11 @@
 class TextType < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '我が子自慢' },
-    { id: 3, name: '我が子紹介' },
-    { id: 4, name: '耳寄り情報' },
-    { id: 5, name: '知恵袋' },
-    { id: 6, name: '闘病' },
-    { id: 7, name: 'その他' }
+    { id: 2, name: '日記' },
+    { id: 3, name: '豆知識' },
+    { id: 4, name: '知恵袋' },
+    { id: 5, name: '闘病' },
+    { id: 6, name: 'その他' }
   ]
 
   include ActiveHash::Associations
