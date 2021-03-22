2517a5401ec8a58e42b93f03624e27f5e31a9e34
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-22 20:22:33 +0900

    予定に過去日を設定できないよう、バリデーション修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ApplicationRecord
 
+  validate :schedule_cannot_be_in_the_past
+
+
   with_options presence: true do
-    validates :datetime
     validates :title
   end
 
@@ -9,6 +11,16 @@ class Task < ApplicationRecord
     validates :task_type_id
     validates :notice_id
   end
+
+
+  def schedule_cannot_be_in_the_past
+    if datetime == nil
+      errors.add(:datetime, "can't be blank.")
+    elsif (task_type_id == 1) && datetime.past?
+      errors.add(:datetime, "can't specify your past date as your '予定'.")
+    end
+  end
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :task_type
