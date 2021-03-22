a4ab20d75ffe9f6a725197b7758b782f7d9b8ec9
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-22 16:39:42 +0900

    model作成・マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_051742) do
+ActiveRecord::Schema.define(version: 2021_03_22_073548) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,17 @@ ActiveRecord::Schema.define(version: 2021_03_19_051742) do
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content"
+    t.integer "task_type", null: false
+    t.integer "notice", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -77,4 +88,5 @@ ActiveRecord::Schema.define(version: 2021_03_19_051742) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "users"
   add_foreign_key "pets", "users"
+  add_foreign_key "tasks", "users"
 end
