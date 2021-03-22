85d2c7a1452ee3f4d801339c64c35144a2158fbb
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-22 19:37:35 +0900

    バリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,14 @@
 class Task < ApplicationRecord
 
+  with_options presence: true do
+    validates :datetime
+    validates :title
+  end
+
+  with_options presence: true, numericality: { message: 'Select' } do
+    validates :task_type_id
+    validates :notice_id
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :task_type
